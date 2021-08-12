f765c721e580475c9a8080864d0802ea8a443bc1
ccalvocm <52244870+ccalvocm@users.noreply.github.com>
2021-08-12 16:07:40 -0400

    Ohiggins

--- Mail_bomber.py
+++ Mail_bomber.py
@@ -24,13 +24,13 @@ mail = outlook.CreateItem(0)
 # asunto del correo
 mail.Subject = 'Invitación Taller Participativo: Estudio de erosión Macrozona centro - sur de Chile para la región de O\'Higgins'
 # asunto ñuble
-mail.Subject = 'Invitación Taller Participativo: Estudio de erosión Macrozona centro - sur de Chile para la región de Ñuble '
+# mail.Subject = 'Invitación Taller Participativo: Estudio de erosión Macrozona centro - sur de Chile para la región de Ñuble '
 
 # cuerpo ohiggins
 mail.HTMLBody = "<html><body> <a href=""https://teams.microsoft.com/registration/aTK-N4lK20SRzUUSwSZIIA,Ao2jz4WK_0y1EcbLWEiQ5A,R4vhq1LzJk2JAKriSPOAqg,_uXfQjtChkqCSR37jQIN9A,kWmJt4SFKUKUtuXEnWnAYw,vV8fuKyy5kyeuN2GvcU-ww?mode=read&tenantId=37be3269-4a89-44db-91cd-4512c1264820""> <img src=""https://raw.githubusercontent.com/ccalvocm/Mail_bomber/main/invitacionOhiggins.jpg"" width=""931.3264346190028"" height=""500""></a></body></html>"
 
 # cuerpo ñuble
-mail.HTMLBody = "<html><body> <a href=""https://teams.microsoft.com/registration/aTK-N4lK20SRzUUSwSZIIA,Ao2jz4WK_0y1EcbLWEiQ5A,R4vhq1LzJk2JAKriSPOAqg,1-D3sndFIk-iLBYQ4WP0rw,q68dddF2LUCZftToYPNnsg,UZ0_fdeXMEC9HlsESIUkcA?mode=read&tenantId=37be3269-4a89-44db-91cd-4512c1264820""> <img src=""https://raw.githubusercontent.com/ccalvocm/Mail_bomber/main/invitacionOhiggins.jpg"" width=""931.3264346190028"" height=""500""></a></body></html>"
+# mail.HTMLBody = "<html><body> <a href=""https://teams.microsoft.com/registration/aTK-N4lK20SRzUUSwSZIIA,Ao2jz4WK_0y1EcbLWEiQ5A,R4vhq1LzJk2JAKriSPOAqg,1-D3sndFIk-iLBYQ4WP0rw,q68dddF2LUCZftToYPNnsg,UZ0_fdeXMEC9HlsESIUkcA?mode=read&tenantId=37be3269-4a89-44db-91cd-4512c1264820""> <img src=""https://raw.githubusercontent.com/ccalvocm/Mail_bomber/main/Nuble/invitacionNUBLE.jpg"" width=""931.3264346190028"" height=""500""></a></body></html>"
 
 # destinatarios
 
